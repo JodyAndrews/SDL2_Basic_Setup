807ff113f2e8bf08f916a42b185b31b3a5cdc94e
Jody Andrews <jodyandrews@me.com>
2018-12-20 22:11:33 +0000

    Initial project file commit

---
+++ README.md
@@ -0,0 +1,47 @@
+#SDL2_Basic_Setup for OSX Mojave
+
+### December 2018
+
+This repo is very simply a basic SDL2 setup and demo that finds and uses the latest brew installs of SDL2 libraries you'd most likely desire for a game.
+
+Don't expect too much :) All it does is demo initialising SDL2, loading a texture as a background, draw a rectangle, ttf with an external font, mixer and play ogg 'music' (some free walking sound I found), and changes the app icon in the Dock, and has a _game loop_ with fixed timing.
+
+The code is rough but should work. Open an issue if there's any problems or suggestions though and I'll take a look asap. Or just fork and call it your own. Include the CMake output in any issue. It's **very** verbose and welcome!
+
+The main reason I wrote this is because PKG_SEARCH_MODULE stopped working for me and was fed up with broken apps, this looked a better cross platform(ish) solution, so this works as a nice testbed on new installs.
+
+## Installation
+
+Xcode Command Line Tools ( if not already installed )
+
+- xcode-select --install
+
+SDL2 Installs
+
+- brew install sdl2
+- brew install sdl2_image
+- brew install sdl2_mixer
+- brew install sdl2_ttf
+
+## Building
+
+I use CLion although it's not required. You should be able to clone this repo and open the root folder in CLion. This will auto run CMake against the CMakeLists.txt file and create a _cmake_build_debug_ folder, within which will be a _bin_ folder and sample resources. Once you've built the project the executable will be compiled to there.
+
+You can either use CLion's debug/run or run the executable in finder. Just use lldb for debugging. There was some kerfuffle about gdb and osx a while back, just go with the flow and use lldb on osx - or let me know why not, genuinely interested.
+
+I'll add a command line build here at some point.
+
+Changes to the root resource folder will require CMake to be ran again as it copies resources to the bin folder, in CLion just use _File -> Reload CMake Project_
+
+## Runtime Oddities
+
+With the SDL installs always make sure you're using either Brew or a framework or you'll receive a warning about not knowing which to use and potentially hit an obnoxious runtime error, this isn't specific to this project, _the code isn't that bad_. I trust brew more, so I would suggest using the brew installs mentioned above and removing the SDL frameworks from /library/frameworks to get around this. Your mileage may vary, but on a clean install I've had no issue with this setup.
+
+## Notes
+
+The Find CMAKE files are written by others, I've tested them and they work fine on OSX Mojave. The only change I've made is to FindSDL2Mixer as it uses the SDL2 lib location for it's search. I've changed this to pick up the location returned from FindSDL2 which means there's a dependency between the two, which also means you can't copy FindSDL2Mixer.cmake to another project and expect it to work. I probably should add a comment to that file.
+
+My understanding of these Find CMAKE files is very slim, so I'll rewrite these for my own use in time and get a better understanding.
+
+Feedback is always very welcome. Happy building :)
+
---
+++ src/main.c
@@ -0,0 +1,241 @@
+#include <printf.h>
+#include <SDL.h>
+#include <SDL_ttf.h>
+#include <SDL_image.h>
+#include <SDL_mixer.h>
+
+// SDL Window
+SDL_Window *_window;
+
+// SDL Renderer to draw to
+SDL_Renderer *_renderer;
+
+// Our default font
+TTF_Font *_font;
+
+// Easy access to a useful color
+SDL_Color white = {255, 255, 255};
+
+// Window size
+int _width = 640;
+int _height = 480;
+
+// Our sample header texture and where we're drawing our header to
+SDL_Rect _headerTextRect;
+SDL_Texture *_headerText;
+
+// Our sample texture
+SDL_Texture *_image;
+
+/**
+ * Initialise SDL2 and output some useful display info
+ */
+void init_sdl() {
+  if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+    printf("[Error] SDL Init : %s \n", SDL_GetError());
+  } else {
+    printf("SDL INITIALISED\n");
+    SDL_DisplayMode dm;
+    SDL_GetCurrentDisplayMode(0, &dm);
+
+    printf("Display mode is %dx%dpx @ %dhz\n", dm.w, dm.h, dm.refresh_rate);
+  }
+}
+
+/**
+ * Initialise an SDL Window and Renderer
+ *
+ * This uses SDL_CreateWindowAndRenderer. They can alternatively be created separately. See SDL2 Docs
+ */
+void init_window_and_renderer() {
+  if (SDL_CreateWindowAndRenderer(_width, _height, SDL_WINDOW_SHOWN, &_window, &_renderer) != 0) {
+    printf("[Error] Creating Window and Renderer: %s\n", SDL_GetError());
+    exit(0);
+  } else {
+    printf("Created Window and Renderer %dx%d\n", _width, _height);
+  }
+}
+
+/**
+ * Initialise TTF
+ */
+void init_ttf() {
+  TTF_Init();
+}
+
+/**
+ * Initialise mixer
+ */
+void init_audio() {
+  if (Mix_OpenAudio( 22050, MIX_DEFAULT_FORMAT, 2, 4096) != 0) {
+    printf("[Error] Error Initialising Audio : %s\n", SDL_GetError());
+  } else {
+    printf("Audio Initialised\n");
+  }
+}
+
+/**
+ * Setup a sample header text
+ */
+void setup_header_text() {
+  // See CMakeLists.txt to see how the resources folder is copied from the root to the bin folder
+  _font = TTF_OpenFont("resources/OpenSans-Regular.ttf", 18);
+
+  SDL_Surface *textSurface = TTF_RenderText_Blended(_font, "Press [Escape] to Exit",
+                                                    white);
+  _headerText = SDL_CreateTextureFromSurface(_renderer, textSurface);
+
+  _headerTextRect.x = _width / 2 - textSurface->w / 2;
+  _headerTextRect.y = 0;
+  _headerTextRect.w = textSurface->w;
+  _headerTextRect.h = textSurface->h;
+
+  SDL_FreeSurface(textSurface);
+
+  // The value will probably be misreported as never used in CLion, however I think it should always be set
+  textSurface = NULL;
+}
+
+/**
+ * Setup a sample texture
+ */
+void setup_texture() {
+  _image = NULL;
+
+  // Load image at specified path
+  SDL_Surface *loadedSurface = IMG_Load("resources/floor.png");
+  if (loadedSurface == NULL) {
+    printf("[Error] Unable to load image : %s\n", SDL_GetError());
+    exit(0);
+  } else {
+    _image = SDL_CreateTextureFromSurface(_renderer, loadedSurface);
+    if (_image == NULL) {
+      printf("[Error] Unable to create texture : %s\n", SDL_GetError());
+    }
+
+    SDL_FreeSurface(loadedSurface);
+  }
+}
+
+/**
+ * Setup a window app icon
+ */
+void setup_window_icon() {
+  SDL_Surface *iconSurface;
+  iconSurface = IMG_Load ("resources/appicon.jpg");
+
+  // The icon requires the window pointer NOT the renderer
+  SDL_SetWindowIcon (_window, iconSurface);
+
+  // ...and can now free the appicon surface
+  SDL_FreeSurface (iconSurface);
+}
+
+/**
+ * Play a sample audio file
+ */
+void play_audio() {
+  Mix_Music *music = NULL;
+  music = Mix_LoadMUS("resources/sound.ogg");
+  if (Mix_PlayMusic(music, -1) != 0) {
+    printf("[Error] Could not play music : %s", Mix_GetError());
+  }
+}
+
+/**
+ * The main game loop. Continues to loop until Escape or an SDL_Quit event occurs
+ */
+void main_loop() {
+  SDL_bool loop = SDL_TRUE;
+  SDL_Event event;
+
+  while (loop) {
+
+    // Allow quiting with escape key by polling for pending events
+    while (SDL_PollEvent(&event)) {
+      if (event.type == SDL_QUIT) {
+        loop = SDL_FALSE;
+      } else if (event.type == SDL_KEYDOWN) {
+        switch (event.key.keysym.sym) {
+          case SDLK_ESCAPE:
+            loop = SDL_FALSE;
+            break;
+          default:
+            loop = SDL_TRUE;
+        }
+      }
+    }
+
+    // Blank out the renderer with all black
+    SDL_SetRenderDrawColor(_renderer, 0, 0, 0, 0);
+    SDL_RenderClear(_renderer);
+
+    // Note that all rendercopys are order specific.
+
+    // Render the sample texture. We could use a source and/or destination rect to render to
+    // but for now we'll just use it as a background
+    SDL_RenderCopy(_renderer, _image, NULL, NULL);
+
+    // Setup and render a sample rectangle
+    SDL_Rect r;
+    r.x = 10;
+    r.y = 10;
+    r.w = 100;
+    r.h = 100;
+
+    SDL_SetRenderDrawColor(_renderer, 255, 255, 255, 1);
+    SDL_RenderDrawRect(_renderer, &r);
+
+    // Render sample text
+    SDL_RenderCopy(_renderer, _headerText, NULL, &_headerTextRect);
+
+    // Present to renderer
+    SDL_RenderPresent(_renderer);
+    SDL_Delay(10);
+  }
+}
+
+/**
+ * Main entry point
+ * @return exit code
+ */
+int main()
+{
+  // SDL Inits
+  init_sdl();
+  init_window_and_renderer();
+  init_ttf();
+  init_audio();
+
+  // Important note. These setup calls require the resources folder to be copied to the
+  // bin folder. This is done via the CMakeList.txt file - see the 'file' call towards
+  // the end. This means any changes to the resources after the initial project load
+  // require the CMake project to be reloaded - in CLion use File -> Reload CMake Project
+
+  // Setup a sample app icon
+  setup_window_icon();
+
+  // Setup a sample text header that we can draw later
+  setup_header_text();
+
+  // Setup a sample image
+  setup_texture();
+
+  // Play an audio file
+  play_audio();
+
+  // Run our main game loop
+  main_loop();
+
+  // When we exit the loop clean up our samples
+  if (Mix_PlayingMusic()) {
+    Mix_HaltMusic();
+  }
+  SDL_DestroyTexture(_headerText);
+  SDL_DestroyTexture(_image);
+
+  // Clean up and exit SDL
+  SDL_DestroyWindow(_window);
+  SDL_Quit();
+  exit(0);
+}
